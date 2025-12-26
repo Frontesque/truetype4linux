b82e16435b3fb98d4a93c1b4dd287d45195b04b9
Kenny <27463495+Frontesque@users.noreply.github.com>
2025-12-25 21:10:32 -0500

    add windows build set

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 #[allow(non_upper_case_globals)]
 
+#[cfg(target_os = "linux")]
 const EXTRACT_DIRECTORY: &str = "./truetype4linux";
-
+#[cfg(target_os = "linux")]
 mod fonts;
+#[cfg(target_os = "linux")]
 mod cabextract;
+#[cfg(target_os = "linux")]
 mod install;
-// mod os_release;
 
+pub fn splash() {
+    println!("  _   _   _  _   _ \n | |_| |_| || | | |\n | __| __| || |_| |\n | |_| |_|__   _| |\n  \\__|\\__|  |_| |_|");
+    println!("   TrueType4Linux");
+    println!("   v{}", env!("CARGO_PKG_VERSION"));
+    println!("   https://github.com/frontesque/truetype4linux");
+    println!("\nAll done!");
+}
+
+#[cfg(target_os = "windows")]
+fn main () {
+    splash();
+}
+
+#[cfg(target_os = "linux")]
 fn main() {
     if cabextract::ensure() == false {
         return println!("Please install cabextract to run truetype4linux.");
@@ -21,11 +37,6 @@ fn main() {
     println!("Installing fonts...");
     install::refresh_font_config();
 
-    // Print End Splash
-    println!("  _   _   _  _   _ \n | |_| |_| || | | |\n | __| __| || |_| |\n | |_| |_|__   _| |\n  \\__|\\__|  |_| |_|");
-    println!("   TrueType4Linux");
-    println!("   v{}", env!("CARGO_PKG_VERSION"));
-    println!("   https://github.com/frontesque/truetype4linux");
-    println!("\nAll done!");
+    splash();
 
 }
